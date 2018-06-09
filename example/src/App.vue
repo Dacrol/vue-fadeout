93c51b376a474b13543732d131a03468648e4812
Dacrol <jonas.ahrens@gmail.com>
2018-06-09 20:24:52 +0200

    Change offset to 1

--- example/src/App.vue
+++ example/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <fadeout text="This text will be truncated because it is longer than 60 characters." :truncate-chars="60" :offset="18"/>
+    <fadeout text="This text will be truncated because it is longer than 60 characters." :truncate-chars="60" :offset="1"/>
     <br />
     <fadeout text="This text won't." :truncate-chars="60"/>
   </div>
